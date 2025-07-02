b09b5a86c02709eff46f931bd7778f6041db26a9
Marc <marc@schopper.rocks>
2025-07-02 08:30:48 +0200

    Add report function to Simulation module to return the robot's current position with examples in documentation

--- lib/toy_robot/simulation.ex
+++ lib/toy_robot/simulation.ex
@@ -111,7 +111,6 @@ defmodule ToyRobot.Simulation do
     {:ok, %{simulation | robot: turned_robot}}
   end
 
-
   @doc """
   Turns the robot right.
 
@@ -139,5 +138,23 @@ defmodule ToyRobot.Simulation do
 
     {:ok, %{simulation | robot: turned_robot}}
   end
+
+  @doc """
+  Returns the robot's current position.
+
+  ## Examples
+
+  iex> alias ToyRobot.{Robot, Table, Simulation}
+  [ToyRobot.Robot, ToyRobot.Table, ToyRobot.Simulation]
+  iex> table = %Table{north_boundary: 4, east_boundary: 4}
+  %Table{north_boundary: 4, east_boundary: 4}
+  iex> simulation = %Simulation{
+  ...>   table: table,
+  ...>   robot: %Robot{north: 0, east: 0, facing: :north}
+  ...> }
+  iex> simulation |> Simulation.report()
+  %Robot{north: 0, east: 0, facing: :north}
+  """
+  def report(%Simulation{robot: robot}), do: robot
 
 end
